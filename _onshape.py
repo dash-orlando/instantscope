34dd22c1ec6f7238c9230a63476f995a31644a8e
Fluvio Lobo <fluvio.lobo@gmail.com>
2020-09-02 16:29:18 -0400

    Deleted unused files

--- _onshape.py
+++ _onshape.py
@@ -1,33 +1,31 @@
 '''
-Instant Scope :: Onshape
+_onshape.py
 
-    This script executes onshape API calls
+Onshape
+    This cript handles the interaction between our main program and
+    Onshape's Python API
 
 
 Madelene Habib
-Fluvio L Lobo Fenoglietto
+Fluvio L. Lobo Fenoglietto
+09/02/2020
 '''
 
-# import modules
-import webbrowser
-import math
+# modules
+from onshape_client import OnshapeElement, Client
+
+# variables
+config_file = '.onshape_client_config.yaml'
+did = '4106f8fea9cf4607edeba1db'
+wid = 'c11cf0ae6ab5e6297d09562d'
+eid = '3340d6f3b50b6e32e22d9a3b'
 
 
-def exportScope( length, height ):
-    
-    # onshape variables
-    did = '4106f8fea9cf4607edeba1db'
-    wid = 'c11cf0ae6ab5e6297d09562d'
-    eid = '3340d6f3b50b6e32e22d9a3b'
+# do things
+client = Client( keys_file=config_file )
+element = OnshapeElement('https://cad.onshape.com/documents/{}/w/{}/e/{}'.format(did,wid,eid)) # create onshape element
+response = client.part_studios_api.export_stl1(did, 'w', wid, eid, _preload_content=False)
 
-    # export STL command
-    cmd = 'stl?'
+## for configuration
 
-    # configuration parameters
-    
-    length = length/1000 #conversion to meters
-    height = height/1000 #
-    #print( length, height )
-    
-    # execute link
-    webbrowser.open( 'https://cad.onshape.com/api/partstudios/d/{}/w/{}/e/{}/{}configuration=length={}+meter;height={}+meter'.format(did,wid,eid,cmd,length,height) )  # Go to example.com
+# configuration=height%3D0.005%2Bmeter%3Blength%3D0.007%2Bmeter ## text for 
