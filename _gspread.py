b0f3a362109549a8a451dc3544aa45c7fc8cabb2
Fluvio Lobo <fluvio.lobo@gmail.com>
2020-09-03 20:21:36 -0400

    Began working on keeping a uniform format across all scripts

--- _gspread.py
+++ _gspread.py
@@ -10,19 +10,37 @@ Fluvio L Lobo Fenoglietto
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-# functions
+# Functions
+# ----------------------------------------------------------- #
+
 def setupClient():
+    '''
+        setupClient()
+        - Creates/opens google sheets client
+    '''
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('users.json', scope)
     client = gspread.authorize(creds)
     return client
 
+# ----------------------------------------------------------- #
+
 def getColumnValues(client, filename, column):
+    '''
+        getColumnValues()
+        - Retrieves all values in a given row
+    '''
     sheet = client.open( filename ).sheet1
     values = sheet.col_values( column )
     return values
 
+# ----------------------------------------------------------- #
+
 def getRowValues(client, filename, row):
+    '''
+        getColumnValues()
+        - Retrieves all values in a given row
+    '''
     sheet = client.open( filename ).sheet1
     values = sheet.row_values( row )
     return values
