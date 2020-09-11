8a0806d13a99e351164f3e56d1922ebb049d0a22
Fluvio Lobo <fluvio.lobo@gmail.com>
2020-09-11 13:25:35 -0400

    Added a browser select window to the vtk2line function/utility in the instantscope app

--- utilities/vtk2line.py
+++ utilities/vtk2line.py
@@ -1,43 +1,44 @@
 '''
 vtk2line
 
-reads line from ugly vtk file...
+    Utility for the InstantScope App
+        - Extracts point coordinates from "line model" VTK file
+        - Stores point coordinates in CSV file
 
+Fluvio L Lobo Fenoglietto
+Madelene Habib
+09-11-2020
 '''
 
 # IMPORT MODULES ---------------------------------------------------------- #
-import  numpy    as np
-
-
-# INPUT VALUES ------------------------------------------------------------ #
-vtkfile         = 'Model.vtk'                                      # change for the filename of 'input' VTK file
-logfile         = 'logfile_Model.csv'
+import  os
+import  tkinter     as tk
+import  numpy       as np
+from    tkinter     import filedialog
 
 # PROGRAM ----------------------------------------------------------------- #
+root = tk.Tk()                                                              # create TK object
+root.withdraw()
 
-#line_index = 0
-#data_record_flag = 0
-#data_record_index = 0
-#sim_step_flag = 0
-#sim_step_index = 0
-#points = []
+vtkfile = filedialog.askopenfilename()                                      # define input VTK file from browser window
+logfile = os.path.splitext(vtkfile)[0]+'.csv'                               # define output CSV file
+
+print( 'Input  VTK file = {}'.format( vtkfile ) )                           # print input and output filenames
+print( 'Output CSV file = {}'.format( logfile ) )                           # ...
 
 with open( vtkfile ) as infile:
 
-    i = 0                                                               # here we initialize a counter
-    point_label_flag = 0                                                # here we create a variable that works as a binary flag
+    i = 0                                                                   # here we initialize a counter
+    point_label_flag = 0                                                    # here we create a variable that works as a binary flag
     points = []
     
     for line in infile:
         line = (' '.join((line.strip('\n')).split())).split(' ')
         
-        if line[0] == 'POINTS':
-            #print('Here!!!')
-            #print( i )                                                  # here we print the value of the counter when we find the label points
-            point_label_flag = 1                                       # here we flip the value of the flag to 1, which marks the beginning of the POINTS section
+        if line[0] == 'POINTS':                                             # here we print the value of the counter when we find the label points
+            point_label_flag = 1                                            # here we flip the value of the flag to 1, which marks the beginning of the POINTS section
         
-        elif point_label_flag == 1:                                       # and then here we can just check for the state of this flag
-            #print( i )
+        elif point_label_flag == 1:                                         # and then here we can just check for the state of this flag
 
             if len(line) == 9:
                 points.append( '{},{},{}'.format(line[0], line[1], line[2]) )
